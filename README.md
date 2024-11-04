50491c86187486643eed39428c7cdaa48608a74b
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Baekjoon Online Judge #2566 in Java 8
+Java 8로 작성한 [<img src="https://static.solved.ac/tier_small/3.svg" height="20" align="center">
+2566 최댓값](https://www.acmicpc.net/problem/2566) 답안입니다.
+
+[문제 풀기](https://www.acmicpc.net/problem/2566) /
+[제출 보기](https://www.acmicpc.net/source/88806236)
---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int r = 0, c = 0, max = 0;
+			for (int i = 1; i <= 9; i++) {
+				for (int j = 1; j <= 9; j++) {
+					int number = scanner.nextInt();
+					if (number > max) {
+						max = number;
+						r = i;
+						c = j;
+					}
+				}
+			}
+			System.out.printf("%d%n%d %d", max, r, c);
+		}
+	}
+}

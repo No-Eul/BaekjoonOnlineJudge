26f55565714a17a84cd9a314ddb51e4f310b01dc
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println(scanner.nextInt() % 7 == 2 ? 1 : 0);
+		}
+	}
+}

f297cb4546242dd19120b050cb99789d00b0b336
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Baekjoon Online Judge #9498 in Java 8
+Java 8로 작성한 [<img src="https://static.solved.ac/tier_small/1.svg" height="20" align="center">
+9498 시험 성적](https://www.acmicpc.net/problem/9498) 답안입니다.
+
+[문제 풀기](https://www.acmicpc.net/problem/9498) / [제출 보기](https://www.acmicpc.net/source/86403918)
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int score = scanner.nextInt();
+			System.out.println(score >= 90 ? 'A'
+					: score >= 80 ? 'B'
+					: score >= 70 ? 'C'
+					: score >= 60 ? 'D'
+					: 'F'
+			);
+		}
+	}
+}

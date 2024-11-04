9fd34494e9c4cfb3be51696a30b876804ceca7a0
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			for (int n = scanner.nextInt(); n > 0; n--)
+				System.out.println("SciComLove");
+		}
+	}
+}

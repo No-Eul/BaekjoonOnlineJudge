043a77fcffe37c4d8455e84a4ad1afe12ce03f20
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Baekjoon Online Judge #2523 in Java 8
+Java 8로 작성한 [<img src="https://static.solved.ac/tier_small/3.svg" height="20" align="center">
+2523 별 찍기 - 13](https://www.acmicpc.net/problem/2523) 답안입니다.
+
+[문제 풀기](https://www.acmicpc.net/problem/2523) /
+[제출 보기](https://www.acmicpc.net/source/88763214)
---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int n = scanner.nextInt();
+			for (int i = 1; i < n; i++) {
+				for (int j = i; j > 0; j--)
+					System.out.print('*');
+				System.out.println();
+			}
+			for (int i = n; i > 0; i--) {
+				for (int j = i; j > 0; j--)
+					System.out.print('*');
+				System.out.println();
+			}
+		}
+	}
+}

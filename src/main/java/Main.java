edfd04ee74114f798305ea0c25279717c8f8ffec
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int sum = 0;
+			for (int i = scanner.nextInt(); i > 0; i--)
+				sum += scanner.nextInt();
+			System.out.println(sum < 0 ? "Left" : sum > 0 ? "Right" : "Stay");
+		}
+	}
+}

34d4bb9d8aa1ed0a0449246998fcb00bc90b87c1
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Baekjoon Online Judge #10952 in Java 8
+Java 8로 작성한 [<img src="https://static.solved.ac/tier_small/1.svg" height="20" align="center">
+10952 A+B - 5](https://www.acmicpc.net/problem/10952) 답안입니다.
+
+[문제 풀기](https://www.acmicpc.net/problem/10952) / [제출 보기](https://www.acmicpc.net/source/86404490)
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (scanner.hasNextInt()) {
+				int a = scanner.nextInt();
+				int b = scanner.nextInt();
+				if (a == 0 && b == 0)
+					break;
+				System.out.println(a + b);
+			}
+		}
+	}
+}

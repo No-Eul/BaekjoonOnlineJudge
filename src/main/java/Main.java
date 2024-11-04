f7b2eaf148abf80a46202a37f385835515817f78
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-05 01:53:45 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int amount = scanner.nextInt();
+			for (int i = 1; i <= amount; i++) {
+				System.out.print(" ".repeat(amount - i));
+				System.out.println("*".repeat(i));
+			}
+		}
+	}
+}

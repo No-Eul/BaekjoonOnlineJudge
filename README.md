9a84769e98bc576eade5983fa22f5b6685621495
NoEul <67686234+no-eul@users.noreply.github.com>
2024-11-06 03:26:56 +0900

    누락된 파일 내용 업데이트 및 README.md 템플릿 양식 추가

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Baekjoon Online Judge Java Template
 [Baekjoon Online Judge](https://www.acmicpc.net) 문제풀이 Java 템플릿 답안 브랜치입니다.
+
+템플릿 작성 시 다음과 같이 작성합니다.
+
+# Baekjoon Online Judge #0000 in Java 8
+Java 8로 작성한 [BOJ 0000번 문제](https://www.acmicpc.net/problem/0000) 답안입니다.
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,17 @@ plugins {
 
 group = "dev.noeul.problemsolving.boj"
 version = "1.0.0"
+
+val targetJavaVersion = JavaVersion.VERSION_1_8
+java {
+	targetCompatibility = targetJavaVersion
+	sourceCompatibility = targetJavaVersion
+	if (JavaVersion.current() < targetJavaVersion)
+		toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion.majorVersion.toInt()))
+}
+
+tasks.compileJava {
+	if (targetJavaVersion >= JavaVersion.VERSION_1_10 || JavaVersion.current().isJava10Compatible)
+		options.release.set(targetJavaVersion.majorVersion.toInt())
+	options.encoding = "UTF-8"
+}

70e436546cc9cd26ff4a1d042be054dd2f586ac0
NoEul <67686234+no-eul@users.noreply.github.com>
2024-12-03 02:43:39 +0900

    제출 소스코드 업데이트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println(-(scanner.nextInt() - scanner.nextInt()));
+			System.out.println(-scanner.nextInt() + scanner.nextInt());
 		}
 	}
 }

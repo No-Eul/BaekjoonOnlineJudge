f1a747d35230c1c1bcc573b6657e2defe846bb0d
NoEul <67686234+no-eul@users.noreply.github.com>
2025-01-18 01:14:31 +0900

    Initial commit

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Java 8로 작성한 [<img src="https://static.solved.ac/tier_small/3.svg" height
 2566 최댓값](https://www.acmicpc.net/problem/2566) 답안입니다.
 
 [문제 풀기](https://www.acmicpc.net/problem/2566) /
-[제출 보기](https://www.acmicpc.net/source/88806236)
+[제출 보기](https://www.acmicpc.net/source/88806268)
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			int r = 0, c = 0, max = 0;
+			int r = 1, c = 1, max = 0;
 			for (int i = 1; i <= 9; i++) {
 				for (int j = 1; j <= 9; j++) {
 					int number = scanner.nextInt();
